0b0eced8d96b85f19ee87f65a1fb9e71beb41bef
bajgerizzz <bajgerizzz@gmail.com>
2026-01-26 19:37:41 +0000

    fix: import seedDatabase to resolve TS2304 error

--- api/_lib/routes/videos.ts
+++ api/_lib/routes/videos.ts
@@ -1,6 +1,7 @@
 import { Router, type Request, type Response } from 'express'
 import { getAuthToken, requireAuth, verifyToken } from '../lib/auth.js'
 import dbConnect from '../lib/db.js'
+import { seedDatabase } from '../lib/seed.js'
 import Video from '../models/Video.js'
 import Profile from '../models/Profile.js'
 import Comment from '../models/Comment.js'
